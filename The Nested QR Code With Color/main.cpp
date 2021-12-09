34f201bf01940617a09e51c636310faa0b944251
RmX <yamasakikenjino@gmail.com>
2021-12-09 15:15:47 +0800

    Initial Commit

---
+++ The Nested QR Code With Color/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  The Nested QR Code With Color
+//
+//  Created by RmX on 2021/12/9.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
